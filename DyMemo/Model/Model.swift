00b2ce6d3c3f853f37c1248ba5985f25ad8621f0
Yuneeee <doyune0707@gmail.com>
2019-07-05 11:58:32 +0900

    3. Navigation Controller 추가, 시작화면 지정 4. Main UI 생성 5. Memo 클래스, TableView 생성

---
+++ DyMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  DyMemo
+//
+//  Created by Doyun on 05/07/2019.
+//  Copyright © 2019 Doyun. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+    Memo(content: "Lorem Ipsum"),
+    Memo(content: "Dolar Amet")
+    ]
+}
